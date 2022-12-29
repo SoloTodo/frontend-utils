00620ac9d82d082e3cac4486155d788202cfc9e1
Vijay Khemlani <vj@solotodo.com>
2022-12-29 11:06:28 -0300

    * Added OpenBox condition

--- conditions.ts
+++ conditions.ts
@@ -3,4 +3,5 @@ export const conditions = [
   { value: "https://schema.org/NewCondition", label: "Nuevo" },
   { value: "https://schema.org/RefurbishedCondition", label: "Reacondicionado" },
   { value: "https://schema.org/UsedCondition", label: "Usado" },
+  { value: "https://schema.org/OpenBoxCondition", label: "Open Box" },
 ];
