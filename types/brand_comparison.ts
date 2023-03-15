1875babbc0c0785a5e20ec954c1bc67c8bb23a1e
Max Edwards <max@solotodo.com>
2023-03-15 17:05:01 -0300

    create segment and row types

--- types/brand_comparison.ts
+++ types/brand_comparison.ts
@@ -2,6 +2,25 @@ import { InLineProduct } from "./entity";
 import { Category } from "./store";
 import { User } from "./user";
 
+export type Row = {
+  id: number;
+  is_product_1_highlighted: boolean;
+  is_product_2_highlighted: boolean;
+  ordering: number;
+  product_1: InLineProduct | null;
+  product_2: InLineProduct | null;
+  segment: string;
+};
+
+export type Segment = {
+  comparison: string;
+  id: number;
+  name: string;
+  ordering: number;
+  rows: Row[];
+  url: string;
+};
+
 export type BrandComparison = {
   brand_1: InLineProduct;
   brand_2: InLineProduct;
@@ -10,6 +29,7 @@ export type BrandComparison = {
   manual_products: InLineProduct[];
   name: string;
   price_type: "offer" | "normal";
+  segments: Segment[];
   stores: string[];
   url: string;
   user: User;
